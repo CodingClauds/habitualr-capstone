94daee4b97babfd5e8561189f8e8985eac0a1905
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-06-30 14:55:49 -0400

    Added the icons in on the buttons

--- habitualr-client/src/pages/HabitPage/HabitPage.js
+++ habitualr-client/src/pages/HabitPage/HabitPage.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Component } from "react";
 import "../HabitPage/HabitPage.scss";
 import returnButton from "../../assets/images/back-60.png";
+import bookIcon from "../../assets/images/book-96.png";
+import sleepIcon from "../../assets/images/sleep-100.png";
+import screenIcon from "../../assets/images/screen-64.png";
+import drinkIcon from "../../assets/images/glass-60.png";
+import exerciseIcon from "../../assets/images/dumbbell-64.png";
+import bedIcon from "../../assets/images/bed-96.png";
+import waterIcon from "../../assets/images/water-bottle-58.png";
+import freetimeIcon from "../../assets/images/leisure-64.png";
 
 class HabitPage extends Component {
   onClick = (e) => {
@@ -29,44 +37,92 @@ class HabitPage extends Component {
           </section> */}
 
           <div className="habit__content">
-            <button className="habit__chosen habit__one" onClick={this.onClick}>
-              Make my Bed
-            </button>
-            <button className="habit__chosen habit__two" onClick={this.onClick}>
-              Sleep Intake
-            </button>
             <button
-              className="habit__chosen habit__three"
+              className="habit__chosen habit__image--one"
               onClick={this.onClick}
             >
-              Limit Screen-Time
+              <img className="habit__icon" src={bookIcon} alt="book" />
+              <span class="habit__btn-text">Read a Book</span>
+              <div class="habit__btnbox"></div>
             </button>
+
             <button
-              className="habit__chosen habit__four"
+              className="habit__chosen habit__image--two"
               onClick={this.onClick}
             >
-              Alcohol Intake
+              <img className="habit__icon" src={sleepIcon} alt="sleep icon" />
+              <span class="habit__btn-text">Sleep</span>
+              <div class="habit__btnbox"></div>
             </button>
+
             <button
-              className="habit__chosen habit__five"
+              className="habit__chosen habit__image--three"
               onClick={this.onClick}
             >
-              Exercise
+              <img className="habit__icon" src={screenIcon} alt="screen icon" />
+              <span class="habit__btn-text">Screen-Time</span>
+              <div class="habit__btnbox"></div>
             </button>
-            <button className="habit__chosen habit__six" onClick={this.onClick}>
-              Read a Book
-            </button>
+
             <button
-              className="habit__chosen habit__seven"
+              className="habit__chosen habit__image--four"
               onClick={this.onClick}
             >
-              Water Intake
+              <img
+                className="habit__icon"
+                src={drinkIcon}
+                alt="glass of wine icon"
+              />
+              <span class="habit__btn-text">Alcohol Intake</span>
+              <div class="habit__btnbox"></div>
             </button>
+
             <button
-              className="habit__chosen habit__eight"
+              className="habit__chosen habit__image--five"
               onClick={this.onClick}
             >
-              Free Time
+              <img
+                className="habit__icon"
+                src={exerciseIcon}
+                alt="dumbell icon"
+              />
+              <span class="habit__btn-text">Exercise</span>
+              <div class="habit__btnbox"></div>
+            </button>
+
+            <button
+              className="habit__chosen habit__image--six"
+              onClick={this.onClick}
+            >
+              <img className="habit__icon" src={bedIcon} alt="bed icon" />
+              <span class="habit__btn-text">Make my Bed</span>
+              <div class="habit__btnbox"></div>
+            </button>
+
+            <button
+              className="habit__chosen habit__image--seven"
+              onClick={this.onClick}
+            >
+              <img
+                className="habit__icon"
+                src={waterIcon}
+                alt="water bottle icon"
+              />
+              <span class="habit__btn-text">Water Intake</span>
+              <div class="habit__btnbox"></div>
+            </button>
+
+            <button
+              className="habit__chosen habit__image--eight"
+              onClick={this.onClick}
+            >
+              <img
+                className="habit__icon"
+                src={freetimeIcon}
+                alt="lesuire icon"
+              />
+              <span class="habit__btn-text">Free Time</span>
+              <div class="habit__btnbox"></div>
             </button>
           </div>
         </section>
