ece4131d302e16d423d3222b5618fdf72b6ab8e8
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-07-01 21:31:12 -0400

    Added About Us page

---
+++ habitualr-client/src/pages/AboutUs/AboutUs.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <>
+      <div className="aboutus__body">
+        <h1 className="aboutus__title">Our Mission Statement</h1>
+
+        <h2 className="aboutus__title">hab·it</h2>
+        <span className="aboutus__noun">noun</span>
+        <p className="aboutus__definition">
+          1. A habit is a routine of behavior that is repeated regularly and
+          tends to occur subconsciously. The American Journal of Psychology
+          defined a "habit, from the standpoint of psychology, [as] a more or
+          less fixed way of thinking, willing, or feeling acquired through
+          previous repetition of a mental experience."
+        </p>
+      </div>
+      <div>
+        <h2 className="aboutus__title">hab·it</h2>
+        <h3 className="aboutus__noun">noun</h3>
+        <p className="aboutus__definition">
+          1. A habit is a routine of behavior that is repeated regularly and
+          tends to occur subconsciously. The American Journal of Psychology
+          defined a "habit, from the standpoint of psychology, [as] a more or
+          less fixed way of thinking, willing, or feeling acquired through
+          previous repetition of a mental experience."
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default AboutUs;
