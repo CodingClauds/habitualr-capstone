eb6d13319299440ac2a425610db37b1deb286639
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-07-01 21:19:23 -0400

    Added new routes

--- habitualr-client/src/App.js
+++ habitualr-client/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
 import HabitPage from "./pages/HabitPage/HabitPage";
 import TrackerPage from "./pages/TrackerPage/TrackerPage";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -14,7 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/habit" component={HabitPage} />
+          <Route path="/motivation" component={HabitPage} />
           <Route path="/tracker" component={TrackerPage} />
+          <Route path="/about-us" component={AboutUs} />
         </Switch>
       </Router>
     </>
