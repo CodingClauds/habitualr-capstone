5e7572dd35a8cc488ad816a8028b0d74239515a8
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-07-05 11:31:46 -0400

    Updating README.md

--- habitualr-client/src/components/Footer/Footer.js
+++ habitualr-client/src/components/Footer/Footer.js
@@ -1,7 +1,36 @@
 import "./Footer.scss";
+import github from "";
+import linkedin from "";
+import instagram from "";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <>
+      <section className="footer">
+        <h2 className="footer__title">Contact Us</h2>
+
+        <div className="footer__social">
+          <img
+            className="footer__handle"
+            src={instagram}
+            alt="instagram icon"
+          />
+          <img className="footer__handle" src={linkedin} alt="linkedin icon" />
+          <img className="footer__handle" src={github} alt="github icon" />
+        </div>
+
+        <div className="footer__acknowledgements">
+          <span>Claudio Miranda</span>
+
+          <Link className="footer__email" href="mailto:info@sproutrinc.com">
+            info@sproutrInc.com
+          </Link>
+          <span>© Sproutr All rights Reserved</span>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Footer;
