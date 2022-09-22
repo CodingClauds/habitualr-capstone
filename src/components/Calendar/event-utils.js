8c957f0df68d86f45a229a394fee114e2876b544
Claudio Miranda <claudio_769@hotmail.com>
2022-09-22 19:18:58 -0400

    confirming date funciton in the utils file works

--- src/components/Calendar/event-utils.js
+++ src/components/Calendar/event-utils.js
@@ -12,11 +12,11 @@ export const INITIAL_EVENTS = [
     title: "Timed event",
     start: todayStr + "T12:00:00",
   },
-  {
-    id: createEventId(),
-    title: "Timed event",
-    start: "2022-06-20" + "T12:00:00",
-  },
+  // {
+  //   id: createEventId(),
+  //   title: "Timed event",
+  //   start: "2022-06-20" + "T12:00:00",
+  // },
 ];
 
 export function createEventId() {
