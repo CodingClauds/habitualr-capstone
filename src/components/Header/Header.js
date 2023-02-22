5eb7a8464487fa32922feb19eae5d0b1640395d4
Claudio Miranda <claudio_769@hotmail.com>
2023-02-22 18:27:10 -0500

    FR-mobile hamburger menu; functional and is toggling as user clicks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import "./Header.scss";
 import brandLogo from "../../assets/images/sprout-tree.svg";
 
 function Header() {
+  const [menuButton, setMenuButton] = useState("");
+  const [hamburger, setIsHamburgerActive] = useState("");
+
+  const toggleMenu = () => {
+    if (menuButton === "") {
+      setMenuButton("is-active");
+      setIsHamburgerActive("is-active");
+    } else {
+      setMenuButton("");
+      setIsHamburgerActive("");
+    }
+  };
   return (
     <>
       <header className="brand">
@@ -14,7 +28,7 @@ function Header() {
             </a>
           </div>
 
-          <nav className="brand__nav">
+          {/* <nav className="brand__nav">
             <ul className="brand__nav-list">
               <li className="brand__nav-item">
                 <Link to="/sign-in" activeclassname="is-active">
@@ -40,15 +54,59 @@ function Header() {
                 </Link>
               </li>
 
-              {/* <li className="brand__nav-item">
+              <li className="brand__nav-item">
                 <Link to="/sign-up" activeclassname="is-active">
                   <p className="brand__bold">Sign Up</p>
                 </Link>
-              </li> */}
+              </li>
             </ul>
-          </nav>
+          </nav> */}
+
+          <button className={`hamburger ${hamburger}`} onClick={toggleMenu}>
+            <div className="bar"></div>
+          </button>
         </div>
       </header>
+      <nav className={`mobile-nav ${menuButton}`}>
+        <ul className="mobile-nav__menu">
+          <li className="menuItem brand__nav-item" onClick={toggleMenu}>
+            <Link
+              to="/sign-in"
+              activeclassname="is-active"
+              className="mobile-navlink"
+            >
+              <p className="brand__bold">Sign In</p>
+            </Link>
+          </li>
+          <li className="menuItem brand__nav-item" onClick={toggleMenu}>
+            <Link
+              to="/habit"
+              activeclassname="is-active"
+              className="mobile-navlink"
+            >
+              <p className="brand__bold">Habits</p>
+            </Link>
+          </li>
+          <li className="menuItem brand__nav-item" onClick={toggleMenu}>
+            <Link
+              to="/calendar"
+              activeclassname="is-active"
+              className="mobile-navlink"
+            >
+              <p className="brand__bold">Calendar</p>
+            </Link>
+          </li>
+          <li className="menuItem brand__nav-item" onClick={toggleMenu}>
+            <Link
+              to="/about-us"
+              activeclassname="is-active"
+              className="mobile-navlink"
+            >
+              <p className="brand__bold">About Us</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
